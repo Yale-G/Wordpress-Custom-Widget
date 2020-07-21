7f68a257f93567839039df62a32f482b9b23a46f
Yale-G <tasosg77@gmail.com>
2020-07-21 16:41:52 +0300

    Add header & footer

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <?php wp_head(); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <title><?php wp_title( '&raquo', true, '' ); ?></title>
+</head>
+<body>
+<div class="wrapper">
+    <div class="header">
+        <div class="complementary-menu">
+            <?php wp_nav_menu(array('theme_location' => 'complementary_menu')); ?>
+        </div>
+        <div class="header-main">
+            <div class="logo">
+            
+            </div>
+            <div class="main-menu">
+            <?php wp_nav_menu(array('theme_location' => 'main_menu')); ?>
+            </div>
+            <div class="category-menu">
+                <?php wp_nav_menu(array('theme_location' => 'category_menu')); ?>
+            </div>
+        </div>
+        
+    </div>
