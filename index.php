e61d246ae29c3ebfb845fb7729b5a692768ce20d
Yale-G <tasosg77@gmail.com>
2020-07-26 16:04:08 +0300

    form login

--- index.php
+++ index.php
@@ -3,6 +3,18 @@
 <div class="main">
 
     <div class="main-content">
+        <div class="form">
+            <form action="" method="post">
+                <label for="username">Username:</label><br>
+                <input type="text" id="username" name="username" value=""><br>
+                <label for="password">Password:</label><br>
+                <input type="password" id="password" name="password" value=""><br>
+                <input type="submit" id="submit" value="Εισοδος">
+            </form>
+            Username <?php echo $_POST["username"]; ?>
+            Password <?php echo $_POST["password"]; ?>
+        </div>
+        
     </div>
 
     <div class="sidebar">
